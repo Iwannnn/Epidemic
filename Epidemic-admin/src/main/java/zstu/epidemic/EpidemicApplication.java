7f7699296a3016e950babd5cd99a3db33457d7bf
iwannnn <iwannnn1206@163.com>
2022-04-10 16:05:20 +0800

    modify index

---
+++ Epidemic-admin/src/main/java/zstu/epidemic/EpidemicApplication.java
@@ -0,0 +1,19 @@
+package zstu.epidemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class EpidemicApplication {
+	public static void main(String[] args) {
+		// System.setProperty("spring.devtools.restart.enabled", "false");
+		SpringApplication.run(EpidemicApplication.class, args);
+		System.out.println("( · - ·)启动成功\n");
+	}
+}
