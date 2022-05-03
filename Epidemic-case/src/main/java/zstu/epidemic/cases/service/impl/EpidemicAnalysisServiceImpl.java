bd5a8c8a00921c40a9dbf0828dadedb2d5fdfc5d
ZCQ <1263213837@qq.com>
2022-05-03 17:19:01 +0800

    5.3 17:18 ServiceImpl错误修改

--- Epidemic-case/src/main/java/zstu/epidemic/cases/service/impl/EpidemicAnalysisServiceImpl.java
+++ Epidemic-case/src/main/java/zstu/epidemic/cases/service/impl/EpidemicAnalysisServiceImpl.java
@@ -6,24 +6,26 @@ import zstu.epidemic.cases.domain.EpidemicCase;
 import zstu.epidemic.cases.domain.EpidemicPatient;
 import zstu.epidemic.cases.mapper.EpidemicAnalysisMapper;
 import zstu.epidemic.cases.mapper.EpidemicCaseHospitalMapper;
+import zstu.epidemic.cases.service.EpidemicAnalysisService;
 import zstu.epidemic.cases.vo.PatientDataVo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class EpidemicAnalysisServiceImpl {
-    @Autowired
-    private EpidemicAnalysisServiceImpl epidemicAnalysisService;
+public class EpidemicAnalysisServiceImpl implements EpidemicAnalysisService {
     @Autowired
     private EpidemicAnalysisMapper epidemicAnalysisMapper;
+
+    @Override
     public ArrayList<EpidemicCase> getCaseListByIllnessName(String illness_name){
         return epidemicAnalysisMapper.getCaseListByIllnessName(illness_name);
     }
+    @Override
     public ArrayList<PatientDataVo> get_patient_info(String illness_name){
         return epidemicAnalysisMapper.get_patient_info(illness_name);
     }
-
+    @Override
     public ArrayList<EpidemicPatient> getPatientListByIllnessName(String illness_name){
         return epidemicAnalysisMapper.getPatientListByIllnessName(illness_name);
     }
