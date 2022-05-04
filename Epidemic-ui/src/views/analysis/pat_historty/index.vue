47937712f0d40c804f6ae435569e5f67d51d948a
Nerachel0 <1393829135@qq.com>
2022-05-04 19:30:58 +0800

    nyh：

--- Epidemic-ui/src/views/analysis/pat_historty/index.vue
+++ Epidemic-ui/src/views/analysis/pat_historty/index.vue
@@ -1,23 +1,55 @@
 <template>
+  <div>
     <div class="search">
-        <el-input v-model="patient_name" placeholder="请输入患者名称" />
-        <el-button @click="reset">重置</el-button>
-        <el-button @click="get_history">确认</el-button>
+      <el-input v-model="patient_name" placeholder="请输入疾病名" />
+      <el-button @click="reset">重置</el-button>
+      <el-button @click="get_history">确认</el-button>
     </div>
+    <!--    <li>{{items}}<li/>-->
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      max-height="300px"
+      :line-numbers="true"
+    >
+    </vue-good-table>
+
+  </div>
 </template>
 
 <script>
+
 import { query_pat_history } from "@/api/analysis/analysis.js";
+import { VueGoodTable } from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css'
+
 export default {
-    data() {
-        return {
-            patient_name: "",
-        };
-    },
+  data() {
+    return {
+      columns: [
+        {
+          label: 'illness_id',
+          field: 'illness_id',
+          type:'int',
+        },
+        {
+          label: 'illness_name',
+          field: 'illness_name',
+          type:'string'
+        },
+      ],
+      rows: [],
+      patient_name: "",
+    };
+  },
+  components: {
+    VueGoodTable,
+  },
     methods: {
         get_history() {
             query_pat_history(this.patient_name).then((res) => {
                 console.log(res);
+                this.rows=res.data;
             });
         },
         reset() {
