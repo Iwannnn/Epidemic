4b44b16b2d5b5a40420abaeb6fe2b6efae3b356c
Nerachel0 <1393829135@qq.com>
2022-05-04 19:30:58 +0800

    nyh：

--- Epidemic-ui/src/views/analysis/drug_illness/index.vue
+++ Epidemic-ui/src/views/analysis/drug_illness/index.vue
@@ -1,23 +1,65 @@
 <template>
+  <div>
     <div class="search">
-        <el-input v-model="illness_name" placeholder="请输入疾病名" />
-        <el-button @click="reset">重置</el-button>
-        <el-button @click="get_drug">确认</el-button>
+      <el-input v-model="illness_name" placeholder="请输入疾病名" />
+      <el-button @click="reset">重置</el-button>
+      <el-button @click="get_drug">确认</el-button>
     </div>
-</template>
+<!--    <li>{{items}}<li/>-->
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      max-height="300px"
+      :line-numbers="true"
+      >
+    </vue-good-table>
 
+  </div>
+</template>
 <script>
+
 import { query_drug } from "@/api/analysis/analysis.js";
+import { VueGoodTable } from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css'
+
 export default {
     data() {
         return {
-            illness_name: "",
+          columns: [
+            {
+              label: 'drug_id',
+              field: 'drug_id',
+              type:'int',
+            },
+            {
+              label: 'company_name',
+              field: 'company_name',
+              type:'string'
+            },
+            {
+              label: 'drug_name',
+              field: 'drug_name',
+              type: 'string'
+            },
+            {
+              label: 'factory_name',
+              field: 'factory_name',
+              type: 'string'
+            },
+          ],
+          rows: [],
+          illness_name: "",
         };
+    },
+    components: {
+      VueGoodTable,
     },
     methods: {
         get_drug() {
             query_drug(this.illness_name).then((res) => {
                 console.log(res);
+                console.log(res.data);
+                this.rows=res.data;
             });
         },
         reset() {
--- Epidemic-ui/src/views/analysis/seq_comp/index.vue
+++ Epidemic-ui/src/views/analysis/seq_comp/index.vue
@@ -1,23 +1,55 @@
 <template>
+  <div>
     <div class="search">
-        <el-input v-model="illness_name" placeholder="请输入疾病名" />
-        <el-button @click="reset">重置</el-button>
-        <el-button @click="get_seq_comp">确认</el-button>
+      <el-input v-model="illness_name" placeholder="请输入疾病名" />
+      <el-button @click="reset">重置</el-button>
+      <el-button @click="get_seq_comp">确认</el-button>
     </div>
+    <!--    <li>{{items}}<li/>-->
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"
+      max-height="300px"
+      :line-numbers="true"
+    >
+    </vue-good-table>
+
+  </div>
 </template>
 
 <script>
+
 import { query_seq_comp } from "@/api/analysis/analysis.js";
+import { VueGoodTable } from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css'
+
 export default {
     data() {
-        return {
-            illness_name: "",
-        };
+      return {
+        columns: [
+          {
+            label: 'illness_id',
+            field: 'illness_id',
+            type:'int',
+          },
+          {
+            label: 'illness_name',
+            field: 'illness_name',
+            type:'string'
+          },
+        ],
+        rows: [],
+        illness_name: "",
+      };
+    },
+    components: {
+      VueGoodTable,
     },
     methods: {
         get_seq_comp() {
             query_seq_comp(this.illness_name).then((res) => {
                 console.log(res);
+                this.rows=res.data;
             });
         },
         reset() {
